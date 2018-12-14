8cceaa1ab2e7e438cf8ceaf4f5f2a6107639470d
Emjeely <emilyumjee@gmail.com>
2018-12-14 14:40:43 -0700

    added a img folder

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+// array of marvel characters
+var topic = ["Stan Lee", "Iron Man ", "Thor", "Captain America", "Deadpool", "Ant-man", "Hulk", "Loki", "spider-man", "Groot"]
+
+// button for 
+var APIkey = "uv1Vh8cL3tiIzgPHtM07w8iSDXIRPHp3"
+var queryURL = ""
